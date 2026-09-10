c22510c178ffaf1cfc8c99655b0369267f6cb7f5
Ebi <167673871+ebi1116@users.noreply.github.com>
2026-09-10 13:50:29 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,6 @@
-from django.shortcuts import render
-from .models import user
+from django.urls import path
+from . import views
 
-
-def login(request):
-
-    if request.method == "POST":
-
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-
-        student = user.objects.get(
-            username=username,
-            password=password
-        )
-
-        return render(request,"profile.html", {"student": student})
-
-    return render(request, "login.html")
+urlpatterns = [
+    path('', views.login)
+]
